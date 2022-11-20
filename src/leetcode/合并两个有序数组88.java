2009552dfab80cc9dbb13ba69d127c03945994d8
alan <763162583@qq.com>
2022-11-20 17:35:13 +0800

    leetcode88

--- src/leetcode/合并两个有序数组88.java
+++ src/leetcode/合并两个有序数组88.java
@@ -7,29 +7,27 @@ package leetcode;
  */
 public class 合并两个有序数组88 {
     public static void main(String[] args) {
-        int[] a = {3,1};
-        int[] b = {5,2};
-        merge(a,2,b,2);
+        int[] a = {1,2,3,0,0,0};
+        int[] b = {2,5,6};
+        a = merge(a,3,b,3);
         for (int i : a) {
             System.out.println(i);
         }
     }
 
-    public static void merge(int[] nums1, int m, int[] nums2, int n) {
-        int[] tempArray = new int[m+n];
-        int i = 0;
-        int p1 = 0;
-        int p2 = 0;
-        while(p1 <= m-1 && p2 <= n-1){
-            tempArray[i] = nums1[p1] >= nums2[p2] ? nums1[p1++] : nums2[p2++] ;
-            i++;
+    public static int[] merge(int[] nums1, int m, int[] nums2, int n) {
+        int p1 = m -1;
+        int p2 = n -1;
+        int i = m + n -1;
+        while(p1 >= 0 && p2 >= 0){
+            nums1[i--] = nums1[p1] >= nums2[p2] ? nums1[p1--] : nums2[p2--];
         }
-        while (p1 <= m-1){
-            tempArray[i++] = nums1[p1++];
+        while (p1 >= 0){
+            nums1[i--] = nums1[p1--];
         }
-        while (p2 <= n-1){
-            tempArray[i++] = nums2[p2++];
+        while (p2 >= 0){
+            nums1[i--] = nums2[p2--];
         }
-        nums1 = tempArray;
+        return nums1;
     }
 }
